e98e2fe98a242824c29a577319fcb08f34ec3d9e
MadalinaDinga <madadinga@gmail.com>
2020-05-28 17:15:12 +0200

    + added password-based authentication

--- vsdk/service_development/models/voiceservice.py
+++ vsdk/service_development/models/voiceservice.py
@@ -12,39 +12,48 @@ from django.utils.translation import ugettext
 from .voicelabel import VoiceLabel, Language, VoiceFragment
 from .vs_element import VoiceServiceElement
 
+
 class VoiceService(models.Model):
     _urls_name = 'service-development:voice-service'
 
-    name = models.CharField(_('Name'),max_length=100)
-    description = models.CharField(_('Description'),max_length=1000)
-    creation_date = models.DateTimeField(_('Date created'), auto_now_add = True)
-    modification_date = models.DateTimeField(_('Date last modified'), auto_now = True)
+    name = models.CharField(_('Name'), max_length=100)
+    description = models.CharField(_('Description'), max_length=1000)
+    creation_date = models.DateTimeField(_('Date created'), auto_now_add=True)
+    modification_date = models.DateTimeField(_('Date last modified'), auto_now=True)
     active = models.BooleanField(_('Voice service active'),
-            help_text = _("A voice service that is active is accessible to users. Marking this service as active (which is only possible when it is valid) will activate this service and disactivate all other services."))
-    _start_element = models.ForeignKey(            
-            VoiceServiceElement,
-            related_name='%(app_label)s_%(class)s_related',
-            verbose_name=_('Starting element'),
-            null = True,
-            on_delete = models.SET_NULL,
-            blank = True)
+                                 help_text=_(
+                                     "A voice service that is active is accessible to users. Marking this service as active (which is only possible when it is valid) will activate this service and deactivate all other services."))
+    _start_element = models.ForeignKey(
+        VoiceServiceElement,
+        related_name='%(app_label)s_%(class)s_related',
+        verbose_name=_('Starting element'),
+        null=True,
+        on_delete=models.SET_NULL,
+        blank=True)
     registration_choices = [('required', _('required (service does not function without Caller ID!)')),
                             ('preferred', _('preferred')),
                             ('disabled', _('disabled'))]
-    registration = models.CharField(_('User registration'),max_length = 15, blank = False, choices = registration_choices)
-    registration_language = models.BooleanField(_('Register Language preference'), help_text= _("The preferred language will be asked and stored during the user registration process"), default = True)
-    registration_name = models.BooleanField(_('Register spoken name'), help_text = _("The user will be asked to speak their name as part of the user registration process"), default = False)
+    registration = models.CharField(_('User registration'), max_length=15, blank=False, choices=registration_choices)
+    registration_language = models.BooleanField(_('Register Language preference'), help_text=_(
+        "The preferred language will be asked and stored during the user registration process"), default=True)
+    registration_name = models.BooleanField(_('Register spoken name'), help_text=_(
+        "The user will be asked to speak their name as part of the user registration process"), default=False)
 
-    supported_languages = models.ManyToManyField(Language, blank = True,verbose_name=_('Supported languages'))
+    supported_languages = models.ManyToManyField(Language, blank=True, verbose_name=_('Supported languages'))
+    is_pass_based_auth = models.BooleanField(_('Password-based authentication'),
+                                             help_text=_(
+                                                 "A voice service that has password-based authentication will identify users by asking for their credentials (username and password)."),
+                                             default=False)
 
     class Meta:
         verbose_name = _('Voice Service')
-    
+
     def get_vxml_url(self):
         try:
-            return reverse(self._urls_name, kwargs ={'voice_service_id': self.id})
+            return reverse(self._urls_name, kwargs={'voice_service_id': self.id})
         except NoReverseMatch:
             return _("unknown")
+
     get_vxml_url.short_description = _("VoiceXML endpoint URL")
     vxml_url = property(get_vxml_url)
 
@@ -54,7 +63,8 @@ class VoiceService(models.Model):
         instead of the VoiceServiceElement superclass object (which does
         not have specific fields and methods).
         """
-        return VoiceServiceElement.objects.get_subclass(id = self._start_element.id)
+        return VoiceServiceElement.objects.get_subclass(id=self._start_element.id)
+
     _get_start_element.short_description = _('Starting element')
     start_element = property(_get_start_element)
 
@@ -63,6 +73,7 @@ class VoiceService(models.Model):
         Returns True if this service supports only a single language
         """
         return len(self.supported_languages.all()) == 1
+
     _supports_single_language.short_description = _('Supports only a single language')
     supports_single_language = property(_supports_single_language)
 
@@ -80,15 +91,16 @@ class VoiceService(models.Model):
     def registration_disabled(self):
         "Returns True if user registration is disabled"
         return self.registration == 'disabled'
-    
-    
+
     def __str__(self):
         return _('Voice Service: %s') % self.name
 
     def is_valid(self):
         if not len(self.validator()) == 0:
             return False
-        else: return True
+        else:
+            return True
+
     is_valid.boolean = True
     is_valid.short_description = _('Is valid')
 
@@ -103,7 +115,6 @@ class VoiceService(models.Model):
         if len(self.supported_languages.all()) == 0:
             errors.append(ugettext('No supported languages'))
 
-        #deduplicate errors
+        # deduplicate errors
         errors = list(set(errors))
         return errors
-
--- vsdk/service_development/views/voiceservice.py
+++ vsdk/service_development/views/voiceservice.py
@@ -1,10 +1,13 @@
+import logging
+
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
-
 from ..models import VoiceService, lookup_or_create_session, lookup_kasadaka_user_by_caller_id
-
 from . import base
+
+logger = logging.getLogger("mada")
+
 
 def get_caller_id_from_GET_request(request):
     if 'caller_id' in request.GET:
@@ -14,23 +17,96 @@ def get_caller_id_from_GET_request(request):
     return None
 
 
-
-def voice_service_start(request, voice_service_id, session_id = None):
-    """
-    Starting point for a voice service. Looks up user (redirects to registation
-    otherwise), creates session, (redirects to language selection).
-    If all requirements are fulfilled, redirects to the starting element of the
-    voice service.
-    """
+def voice_service_start(request, voice_service_id, session_id=None):
+    logger.debug("Voice service with id {} - Request {} - Session id {}".format(voice_service_id, request, session_id))
     voice_service = get_object_or_404(VoiceService, pk=voice_service_id)
 
-    #if not voice_service.active:
+    if not voice_service.active:
         # TODO give a nicer error message
-        #raise Http404()
+        raise Http404("Inactive service")
 
     caller_id = get_caller_id_from_GET_request(request)
     session = lookup_or_create_session(voice_service, session_id, caller_id)
-    session_id = session.id
+    logger.debug("Caller ID {} - Session {}".format(caller_id, session))
+
+    if voice_service.is_pass_based_auth:
+
+        logger.debug("Password-based authentication is set")
+        if not session.language:
+            return select_session_language(voice_service, session, caller_id)
+        logger.debug("Session language {}".format(session.language))
+
+        logger.debug("Start authentication process if required or preferred")
+        if not session.user and voice_service.registration_preferred_or_required:
+            logger.debug("User authentication started")
+            return password_based_auth(session)
+        logger.debug("User authentication finished")
+    else:
+        logger.debug("CallerID-based authentication is set")
+        if voice_service.registration_preferred_or_required:
+            logger.debug("User authentication started")
+            redirect_url = callerID_auth(voice_service, session, caller_id)
+            logger.debug("Redirect url".format(redirect_url))
+            if redirect_url is not None:
+                return redirect_url
+        logger.debug("User authentication finished")
+
+        # If not set, select language
+        if not session.language:
+            logger.debug("Perform language selection")
+            return select_session_language(voice_service, session, caller_id)
+        logger.debug("Session language is {}".format(session.language))
+
+    logger.debug("Redirecting to start element {}".format(voice_service.start_element))
+    return base.redirect_to_voice_service_element(voice_service.start_element, session)
+
+
+def select_session_language(voice_service, session=None, caller_id=None):
+    """
+        Starting point for a voice service. Looks up user (redirects to registation
+        otherwise), creates session, (redirects to language selection).
+        If all requirements are fulfilled, redirects to the starting element of the
+        voice service.
+    """
+    logger.debug(
+        "Language selection started for Voice service {} - Session {} - CallerID {}".format(voice_service, session,
+                                                                                            caller_id))
+
+    # Redirect the user to language selection for this session only.
+    # Make sure to return to start of voice service after selection of language
+    return_url = reverse('service-development:voice-service', args=[session.service.id, session.id])
+    return base.redirect_add_get_parameters('service-development:language-selection',
+                                            session.id,
+                                            redirect_url=return_url)
+
+
+def password_based_auth(session):
+    # Authenticates a user and links the session to the user if auth was successful (valid credentials were given)
+    logger.debug("Authenticated user {}".format(session.user))
+    logger.debug("Current session information {}".format(session))
+    return_url = reverse('service-development:voice-service', args=[session.service.id, session.id])
+    return base.redirect_add_get_parameters('service-development:kasadaka-user-auth',
+                                            session.id,
+                                            redirect_url=return_url)
+
+
+def callerID_auth(voice_service, session=None, caller_id=None):
+    """
+        Starting point for a voice service. Looks up user (redirects to registation
+        otherwise), creates session, (redirects to language selection).
+        If all requirements are fulfilled, redirects to the starting element of the
+        voice service.
+    """
+    logger.debug(
+        "CallerID-based authentication started for Voice service {} - Session {} - CallerID {}".format(voice_service,
+                                                                                                       session,
+                                                                                                       caller_id))
+    # If there is no caller_id provided, and user registration is required for this service,
+    # throw an error
+    if voice_service.registration_required and not caller_id:
+        # TODO make this into a nice audio error
+        raise ValueError(
+            'This service requires registration, but registration is not possible, because there is no callerID!')
 
     # If the session is not yet linked to an user, try to look up the user by
     # Caller ID, and link it to the session. If the user cannot be found,
@@ -39,26 +115,9 @@ def voice_service_start(request, voice_service_id, session_id = None):
         found_user = lookup_kasadaka_user_by_caller_id(caller_id, session.service)
         if found_user:
             session.link_to_user(found_user)
-
+            logger.debug("Session user {}".format(session.user))
+            return None
         # If there is no user with this caller_id and registration of users is preferred or required, redirect to registration
-        elif voice_service.registration_preferred_or_required:
-            return redirect('service-development:user-registration',
-                    session.id)
-
-    # If there is no caller_id provided, and user registration is required for this service,
-    # throw an error
-    elif voice_service.registration_required and not caller_id:
-        # TODO make this into a nice audio error
-        raise ValueError('This service requires registration, but registration is not possible, because there is no callerID!')
-
-
-
-    # If the language for this session can not be determined,
-    # redirect the user to language selection for this session only.
-    if not session.language:
-        # After selection of language, return to start of voice service.
-        return_url = reverse('service-development:voice-service', args = [session.service.id,session.id])
-        return base.redirect_add_get_parameters('service-development:language-selection',
-                        session.id,
-                        redirect_url = return_url)
-    return base.redirect_to_voice_service_element(voice_service.start_element, session)
+        else:
+            logger.debug("Register caller id {}".format(caller_id))
+            return redirect('service-development:user-registration', session.id)
